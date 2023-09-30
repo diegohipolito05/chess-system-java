d8c936966b17ead5fd28b86daf806a1313c94aa1
Diego Hipolito <diegohipolito05@hotmail.com>
2023-09-30 12:34:42 -0300

    Printing possible moves

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -24,8 +24,7 @@ public class Rook extends ChessPiece {
 		
 		//above
 		p.setValues(position.getRow()-1,position.getColumn());
-		boolean x  = getBoard().thereIsAPiece(p);
-		while(getBoard().positionExists(p)&&!x) {
+		while(getBoard().positionExists(p)&&!getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setRow(p.getRow()-1);
 		}
